a727daf257faa2abbcea4b6bf3ea6140cb31df0c
NMRenjoyer <dcreetz+github@proton.me>
2025-10-25 22:41:58 -0500

    Delete basicUtils directory

--- basicUtils/shells.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }:
-{
-  environment.shells = with pkgs; [ zsh bash ];
-  users.defaultUserShell = pkgs.zsh;
-  programs.zsh.enable = true; # zsh is managed by home-manager, this setting is still required.
-}
